b7c8c75b36790aea27c9da4cf0a705beeda1909e
NkAndre <andre270708@gmail.com>
2026-05-06 15:15:17 -0300

    style de frases feito

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable,Image } from 'react-native';
-import React, { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import React, { useState } from "react";
 
-const frases = [ 
+// array de frases , quer adiciona as suas? coloque aqui 
+const frases = [
   "Porque para Deus nada é impossivel",
   "Nunca desista dos seus sonhos",
   "Sempre haverá uma nova chance",
-  "Acredite em si próprio e todo o resto virá naturalmente.",
- 
-]
+  "Acredite em si próprio",
+];
 
 export default function App() {
-  const [frase, setFrase] = useState(frases[0]) // state atual para controlar as nossas frases
+  const [frase, setFrase] = useState(frases[0]); // state atual para controlar as nossas frases
 
   // função para gerar frases aleatorias
   const gerarFrase = () => {
-    const indiceAleatorio = Math.floor(Math.random() * frases.length); 
+    const indiceAleatorio = Math.floor(Math.random() * frases.length);
     setFrase(frases[indiceAleatorio]);
-  } 
+  };
   return (
     <View style={styles.container}>
-      <Text style = {styles.textTitulo}>frases do dia </Text>
+      <Text style={styles.textTitulo}>frases do dia </Text>
 
       <View style={styles.card}>
         <Text style={styles.textoFrase}>"{frase}"</Text>
       </View>
       <View>
-        <Pressable style = {styles.botao} onPress={gerarFrase}>
+        <Pressable style={styles.botao} onPress={gerarFrase}>
           <Text style={styles.textoBotao}>Nova Frase</Text>
         </Pressable>
-        
-        
       </View>
-
 
       <StatusBar style="auto" />
     </View>
@@ -42,24 +39,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ADD8E6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:20,
+    backgroundColor: "#ADD8E6",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
   },
-  textTitulo:{
+  textTitulo: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    color: "#333"
-  } , 
-  card:{
-    backgroundColor:"#fff",
-    padding:20,
-    borderRadius:20,
-    marginVertical:20,
-    elevation:3,
-     width: '100%',
-     alignItems:"center",
-  }
+    color: "#333",
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 20,
+    marginVertical: 20,
+    elevation: 2,
+    width: "100%",
+    alignItems: "center",
+    shadowColor:"#000",
+    shadowOffset: {
+      width:0,
+      height:2
+    },
+    shadowOpacity:0.2
+  },
 });
